6ebddf3532424cbb50fa956d6d55240c36108a82
Rohan Verma <rohanvrm3@gmail.com>
2017-07-30 12:51:03 +0530

    Add files via upload
    Learned operators in JavaScript and wrote my first code to compare age of 3 persons and print results based on conditions

---
+++ Age_Compare_Game_LearningOperators/script.js
@@ -0,0 +1,114 @@
+//this is spartaa
+//console.log(name);
+
+/*
+
+var name = 'John';
+console.log(name);
+
+var lastName = "Smith"; //camelcase
+console.log(lastName);
+
+var age = '26';
+console.log(age);
+
+var fullAge = "true";
+console.log(fullAge);
+
+// lecture: variables 2
+var name = 'John';
+var age = 26;
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+console.log(job);
+job = 'teacher';
+isMarried = 'false';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+// lecture operators
+
+NowAge=21
+var ageJohn = 36;
+var ageRam = 36;
+
+var ageRam = NowAge * ageJohn*2 * (ageJohn - ageRam);
+console.log(ageRam);
+
+console.log(NowAge);
+
+// Lecture: If/Else statements
+
+
+var name = "Rohan";
+var age = 26;
+var isMarried = "no";
+
+if (isMarried == "yes")
+  {
+    console.log(name + " is married");
+  }
+  else
+{
+  console.log(name + " is not married yet");
+}
+
+isMarried = true;
+
+if (isMarried)
+{
+  console.log('yes');
+}
+
+
+// lecture: Boolean operators and switch case
+
+var age=25;
+
+if (age<10)
+{console.log('teenager');
+} else if (age < 20 || age > 30)
+{console.log('John is a young guy');}
+else {
+  console.log('Big guy');
+}
+
+var dance = 'salsa';
+
+dance = prompt('what type of dance is this');
+
+switch (dance) {
+  case 'salsa':
+  console.log('This is salsa dance');
+  break;
+  case 'kathak':
+  console.log('this is sparta');
+    break;
+  default:
+  console.log('bitches');
+
+}
+
+*/
+
+// coding challenge 1
+
+var johnAge = 111; var johnHeight = 120;
+var paulAge = 111; var paulHeight = 120;
+var kevinAge = 111; var kevingHeight = 120;
+
+var johnR = johnAge + (5 * johnHeight);
+var paulR = paulAge + (5 * paulHeight);
+var kevinR = kevinAge + (5 * kevingHeight);
+
+if (johnR > paulR && johnR >= kevinR) {
+  console.log('Yay! John wins.' + ' John score is: ' + johnR);
+} else if (paulR > johnR && paulR >= kevinR ){
+  console.log('Yay! Paul wins. Paul score is: ' + paulR);
+} else if ( kevinR > johnR && kevinR >= paulR ) {
+  console.log('Yay! Kevin wins. Kevis score is: ' + kevinR);
+} else {
+  console.log('it\'s is a draw!');
+}
